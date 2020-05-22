3910909f7fa832806ddf7ca863e55b1435d6866e
Bruno Foggia <bruno@vixs.com.br>
2020-05-21 21:08:31 -0300

    added feature to use deepsearch with json numeric lists (array like)

--- src/underscore-99xp.js
+++ src/underscore-99xp.js
@@ -30,7 +30,7 @@ _.templateSettings = {
 // Traverses the children of obj along path.
 // If a child is a function, it is invoked with given context and arguments.
 // Returns the value of the final child, or fallback if any child is undefined.
-_.result2 = function (obj, key, defaultValue, args, context) {
+_.result2 = function(obj, key, defaultValue, args, context) {
     var v;
     if (!args && !context) {
         v = _.result(obj, key, defaultValue);
@@ -60,19 +60,19 @@ _.result2 = function (obj, key, defaultValue, args, context) {
 
 // Fill in a given object with default properties.
 // The object of this 2nd version is to work with not plain objects
-_.defaults2 = function (o, d) {
+_.defaults2 = function(o, d) {
     var k1 = [],
-            k2 = [];
+        k2 = [];
     var kd1 = [],
-            kd2 = [];
+        kd2 = [];
 
     _.map(o, (i, k) => _.isJSON(i) ? k2.push(k) : k1.push(k));
     _.map(d, (i, k) => _.isJSON(i) ? kd2.push(k) : kd1.push(k));
 
     var o1 = _.partial(_.pick, o).apply(null, k1),
-            o2 = _.partial(_.pick, o).apply(null, k2);
+        o2 = _.partial(_.pick, o).apply(null, k2);
     var od1 = _.partial(_.pick, d).apply(null, kd1),
-            od2 = _.partial(_.pick, d).apply(null, kd2);
+        od2 = _.partial(_.pick, d).apply(null, kd2);
 
     o1 = _.defaults(o1, od1);
     if (_.size(od2)) {
@@ -98,45 +98,57 @@ _.defaults2 = function (o, d) {
 //     _.deepValueSearch('contacts', json) = [ {email: 'team@99xp.org'} , {email: 'admin@99xp.org'} ]
 //     _.deepValueSearch('contacts[]', json) = [ {email: 'team@99xp.org'} , {email: 'admin@99xp.org'} ]
 
-_.deepValueSearch = function (k, json) {
-    var p = typeof k === 'string' && k ? k.split(/\[/) : ((k instanceof Array) ? k : []);
+_.deepValueSearch = function(k, json) {
+    var p = typeof k === 'string' && k ? k.split(/\[/) : k instanceof Array ? k : [];
 
     if (!p.length) {
         return json;
     }
+
     var pk = p.shift();
 
     if (/^(\w|\_|\-)+$/.test(pk)) {
         return this.deepValueSearch(p, json[pk]);
     }
+
     if (pk === ']') {
         if (!p.length) {
             return json;
         }
-        if ((json instanceof Array)) {
-            var r = [];
-            for (let x in json) {
-                r.push(this.deepValueSearch(_.clone(p), _.clone(json[x])));
+
+        if (json instanceof Array || _.isJSON(json)) {
+            var r = _.isJSON(json) ? {} : [];
+
+            for (var x in json) {
+                if (_.isJSON(r)) {
+                    r[x] = (this.deepValueSearch(_.clone(p), _.clone(json[x])));
+                } else {
+                    r.push(this.deepValueSearch(_.clone(p), _.clone(json[x])));
+                }
             }
+
             return r;
         }
     }
+
     if (/(\w|\_|\-)+\]$/.test(pk)) {
         pk = pk.replace(']', '');
+
         if (!p.length) {
             return json[pk];
         }
+
         return !json ? json : this.deepValueSearch(p, json[pk]);
     }
-}
+}; // Checks if given object is not an array
 
 // Checks if given object is not an array
-_.isOnlyObject = function (o) {
+_.isOnlyObject = function(o) {
     return _.isObject(o) && !_.isArray(o);
 };
 
 // Checks if given object is pure js object, not instance of any class
-_.isJSON = function (o) {
+_.isJSON = function(o) {
     return _.isOnlyObject(o) && o.__proto__.__proto__ === null;
 }
 
@@ -145,9 +157,9 @@ _.isJSON = function (o) {
 // --------------
 
 // Capture all occurrences in a string
-_.matchAll = function (s, regexp) {
+_.matchAll = function(s, regexp) {
     var matches = [];
-    s.replace(regexp, function () {
+    s.replace(regexp, function() {
         var arr = ([]).slice.call(arguments, 0);
         var extras = arr.splice(-2);
         arr.index = extras[0];
@@ -158,15 +170,15 @@ _.matchAll = function (s, regexp) {
 };
 
 // Locate index of something into a string based on regexp
-_.regexIndexOf = function (s, regex, startpos) {
+_.regexIndexOf = function(s, regex, startpos) {
     var indexOf = s.substring(startpos || 0).search(regex);
     return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
 };
 
 // Locate last index of something into a string based on regexp
-_.regexLastIndexOf = function (s, regex, ignoreAfterPos) {
+_.regexLastIndexOf = function(s, regex, ignoreAfterPos) {
     regex = (regex.global) ? regex :
-            new RegExp(regex.source, 'g' + (regex.ignoreCase ? 'i' : '') + (regex.multiLine ? 'm' : ''));
+        new RegExp(regex.source, 'g' + (regex.ignoreCase ? 'i' : '') + (regex.multiLine ? 'm' : ''));
 
     if (typeof (ignoreAfterPos) === 'undefined') {
         ignoreAfterPos = s.length;
@@ -185,15 +197,15 @@ _.regexLastIndexOf = function (s, regex, ignoreAfterPos) {
 };
 
 // Transform string into date
-_.toDate = function (s) {
+_.toDate = function(s) {
     if (!/^\d{4}\-\d{2}/.test(s)) {
         return false;
     }
 
     var data = s.toString().split(_.regexIndexOf(s.toString(), /T/) > 0 ? 'T' : ' '),
-            dateStr = data[0].split('-'),
-            timeStr = data[1] ? data[1].split(':') : [0, 0, 0],
-            date;
+        dateStr = data[0].split('-'),
+        timeStr = data[1] ? data[1].split(':') : [0, 0, 0],
+        date;
 
     date = new Date(dateStr[0], parseInt(dateStr[1] || 0, 10) - 1, parseInt(dateStr[2] || 1, 10), timeStr[0] || 0, timeStr[1] || 0, timeStr[2] || 0);
     return date;
